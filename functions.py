fffc908363a8a6e16799befeb7fde436ea04c64b
Mattala <mattalak@vcontractor.corp>
2020-04-22 09:08:42 +0200

    about to start with Flask

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 def square(x):
     return x * x
 
-for index in range(10):
-    print("{} sqaured is {}".format(index,square(index)))
+def main():
+    for index in range(10):
+         print("{} sqaured is {}".format(index,square(index)))
+
+if __name__ == '__main__':
+    main()
---
+++ module.py
@@ -0,0 +1,5 @@
+from functions import  square
+
+#function square is read from the functin.py
+
+print(square(2))
